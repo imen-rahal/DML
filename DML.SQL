9fcd1161dea338ac01177cd6afad0b5271b82f6b
imen-rahal <imenerahal96@gmail.com>
2021-12-07 15:43:59 +0100

    dml

---
+++ DML.SQL
@@ -0,0 +1,11 @@
+INSERT INTO Product VALUES (‘P01’,’Samsung Galaxy S20’,’Smartphone’, 3299) ;
+
+INSERT INTO Product VALUES (‘P02’,’ASUS Notebook’, ‘PC’, 4599) ;
+
+INSERT INTO Customer VALUES (‘C01’,’ALI’,71321009) ;
+
+INSERT INTO Customer VALUES (‘C02’,’ASMA’,773450823) ;
+
+INSERT INTO Orders VALUES (‘C01’,’P02’, NULL, 2,9198) ;
+
+INSERT INTO Orders VALUES (‘C02’,’P01’, ’2020-05-28’, 1,3299) ;
